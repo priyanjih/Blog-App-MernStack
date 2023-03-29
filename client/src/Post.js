14187a40384c6ab3a8fe2d0473c3fc05a67a0eb5
Priyanjith Priyanga <hmjack327@gmail.com>
2023-03-29 15:43:54 +0530

    structure of the index, routing, login and register page.

---
+++ client/src/Post.js
@@ -0,0 +1,24 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1674512540096-46b2ca19ef96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+          alt=""
+        />
+      </div>
+      <div className="text">
+        <h1>parking lot full of cars</h1>
+        <p className="info">
+          <a className="author">daiv</a>
+          <time>2002-11-31 16:34</time>
+        </p>
+        <p className="summery">
+          Download and use 50000+ Car stock photos for free. ✓ Thousands of new
+          images every day ✓ Completely Free to Use ✓ High-quality videos and
+          images from ...
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/pages/IndexPages.js
@@ -0,0 +1,11 @@
+import Post from "../Post";
+
+export default function indexPages() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
